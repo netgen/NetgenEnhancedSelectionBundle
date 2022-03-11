27714c7a34506c98d3a21cc622fbe95e8db1d360
Sven Kovacic <sven@netgen.io>
2022-03-11 14:34:58 +0100

    MDX21-46 fix enhanced selection not loading properly

--- bundle/Resources/public/admin/js/init.js
+++ bundle/Resources/public/admin/js/init.js
@@ -1,5 +1,20 @@
 (function($) {
     'use strict';
-
     $('.multientry').multientry();
+
+    const observer = new MutationObserver(e => {
+        $('.multientry').multientry();
+    });
+
+    document.addEventListener("drop", e => {
+        document.querySelectorAll('.ibexa-collapse').forEach(el => {
+            observer.observe(el, { childList: true, subtree: true });
+        });
+    });
+
+    document.addEventListener("drag", e => {
+        document.querySelectorAll('.ibexa-collapse').forEach(el => {
+            observer.disconnect();
+        });
+    });
 })(jQuery);
