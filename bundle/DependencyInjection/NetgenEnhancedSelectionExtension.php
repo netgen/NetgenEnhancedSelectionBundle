2883d4e60594e8ab2326dc238265a90272d171d0
Edi Modrić <edi.modric@gmail.com>
2025-12-10 14:42:10 +0100

    Do not use deprecated DI Extension class

--- bundle/DependencyInjection/NetgenEnhancedSelectionExtension.php
+++ bundle/DependencyInjection/NetgenEnhancedSelectionExtension.php
@@ -7,9 +7,9 @@ namespace Netgen\Bundle\EnhancedSelectionBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Yaml\Yaml;
 
 use function array_keys;
